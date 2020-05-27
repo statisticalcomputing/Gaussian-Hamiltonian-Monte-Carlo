e7d7fda1a2ed8dcb3f88ccedeccdce0b6488e718
xp <xiaopeng.xu.win@gmail.com>
2020-05-27 11:48:23 -0500

    Add files via upload

--- hmc_vanilla.py
+++ hmc_vanilla.py
@@ -1,5 +1,3 @@
-''' stochastic hmc, delta and E_total from small to large, never decrease
-'''
 import numpy as np
 from math import sqrt, exp, log
 from numpy.random import randn, rand
@@ -7,9 +5,10 @@ from numpy.linalg import norm
 import matplotlib.pyplot as plt
 from argmin import argmin
 import gc
-
+from math import log
 def hmc(U, dU, x0= None,D=None, EPISODE=10000, BURNIN=None,VERBOSE=False,L=5, delta=.05,WRONG=False):
 
+    
     if BURNIN is None:
         BURNIN = int(EPISODE/2)
 
@@ -60,39 +59,48 @@ if __name__ == '__main__':
            .5,
            .6,
            .7,
-           .8
+           .8,
+           .9
     ]
-    DELTA = [0.01,0.02, 0.04, 0.08, 0.16, 0.32, 0.64]
+    DELTA = [0.01,0.02, 0.04, 0.08, 0.16, 0.32]
     DS = []
     DS1 = []
+    plt.ion()
     #np.random.seed(12345)
-    for delta in DELTA:
+    for i in range(len(DELTA)):
+        delta = DELTA[i]
         ds = []
         ds1 = []
         print(delta)
-        for i in range(len(rho)):
-            r = rho[i]
+        for j in range(len(rho)):
+            r = rho[j]
+            #print('r',r)
             SIGMA = np.array([[1, r],[r, 1]])
             U = lambda x: np.sum(x * np.linalg.solve(SIGMA,x), axis = 0)/2
             dU = lambda x: np.linalg.solve(SIGMA, x)
 
-            info = hmc(U, dU, D=D, EPISODE=EPISODE, WRONG=True)
+            info = hmc(U, dU, D=D, delta=delta,EPISODE=EPISODE, WRONG=True)
+            x = info['x']
+            d0_ = np.sum(np.square(np.cov(np.transpose(x[int(EPISODE/2):,:])) - SIGMA))
+            ds.append(log(d0_))
+            info = hmc(U, dU, D=D, delta=delta, EPISODE=EPISODE, WRONG=False)
             x = info['x']
             d0 = np.sum(np.square(np.cov(np.transpose(x[int(EPISODE/2):,:])) - SIGMA))
-            ds.append(d0)
-            info = hmc(U, dU, D=D, EPISODE=EPISODE, WRONG=False)
-            x = info['x']
-            d0 = np.sum(np.square(np.cov(np.transpose(x[int(EPISODE/2):,:])) - SIGMA))
-            ds1.append(d0)
+            ds1.append(log(d0))
+            print(r, d0_, d0)
         DS.append(ds)
         DS1.append(ds1)
-    for i in range(len(DELTA)):
-        plt.plot(DS[i],'--')
-        plt.plot(DS1[i],'-')
+        plt.plot(ds,'--')
+        plt.plot(ds1,'-')
+        plt.show()
+        plt.pause(0.1)
+    # for i in range(len(DELTA)):
+    #     plt.plot(DS[i],'--')
+    #     plt.plot(DS1[i],'-')
     plt.xlabel('rho')
     plt.xticks(list(range(len(rho))), [str(r) for r in rho], rotation=90)
 
-    plt.ylabel(r'$\left|\Sigma - \hat \Sigma\right|$')
+    plt.ylabel(r'log $\left|\Sigma - \hat \Sigma\right|$')
     plt.legend(['{}/{}'.format(r,d) for d in DELTA for r in ['hmc','corrected'] ])
     plt.show()
     plt.pause(10000)
